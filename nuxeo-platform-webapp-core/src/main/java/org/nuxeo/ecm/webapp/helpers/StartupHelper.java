5cdabe7aa7271e51d560e36e74f44f644536afb9
Sun Seng David TAN <stan@nuxeo.com>
2012-06-19 09:57:30 +0200

    NXP-9417 Commit back failing merge

--- nuxeo-platform-webapp-core/src/main/java/org/nuxeo/ecm/webapp/helpers/StartupHelper.java
+++ nuxeo-platform-webapp-core/src/main/java/org/nuxeo/ecm/webapp/helpers/StartupHelper.java
@@ -35,6 +35,7 @@ import org.jboss.seam.annotations.Install;
 import org.jboss.seam.annotations.Name;
 import org.jboss.seam.annotations.Scope;
 import org.jboss.seam.contexts.Context;
+import org.jboss.seam.core.Events;
 import org.jboss.seam.international.LocaleSelector;
 import org.nuxeo.ecm.core.api.ClientException;
 import org.nuxeo.ecm.core.api.CoreSession;
