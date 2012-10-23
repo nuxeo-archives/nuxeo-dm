d15eaaf6485f5e20b5aa7919cb795e9e1a5d852d
Thierry Delprat <tdelprat@nuxeo.com>
2012-10-23 13:56:32 +0200

    NXP-9383 improve key binding helper popup

--- nuxeo-platform-webapp/src/main/resources/web/nuxeo.war/scripts/accessKey.js
+++ nuxeo-platform-webapp/src/main/resources/web/nuxeo.war/scripts/accessKey.js
@@ -1,17 +1,52 @@
 function showAccessKeys() {
-    if (jQuery(".accessKeyToolTip").size()>0) {
-        jQuery(".accessKeyToolTip").remove();
+    if (jQuery(".accessKeyMenu").size()>0) {
+        jQuery(".accessKeyMenu").remove();
         return;
     }
+    var container = jQuery("<div></div>");
+    container.css("display","none");
+    var div = jQuery("<div></div>");
+    div.attr("id","accessKeyMenuPopup");
+    div.addClass("accessKeyMenu");
+    div.css({"padding":"8px", "margin":"4px", "font-size" : "12px"});              
+    container.append(div);
+
+    var table = jQuery("<table></table>");
+    div.append(table);
+
     jQuery("[accesskey]").each(function() {
         var item = jQuery(this);
         var key = item.attr("accesskey");
-        if (key !=null && key !="" && this.innerHTML!="") {
-              var tooltip = jQuery("<span>" + key + "</span>");
-              jQuery(tooltip).css({"background-color":"#666666", "color":"white","padding":"3px", "margin":"2px","border-radius" : "2px", "font-size" : "9px"});
-              jQuery(tooltip).addClass("accessKeyToolTip");
-              item.append(tooltip);
+
+
+        if (key !=null && key !="") {              
+              var row = jQuery("<tr></tr>");
+              var keySpan = jQuery("<span>" + key + "</span>");                 
+              keySpan.css({"background-color":"#666666", "color":"white","padding":"3px", "margin":"2px","border-radius" : "2px", "font-size" : "12px"});              
+
+              var keyText = this.innerHTML;
+              if (this.tagName=="INPUT" && this.attr("type")=="button") {
+                 keyText = this.attr("value");
+              }
+              if (keyText) {
+                var td = jQuery("<td></td>");
+                td.css({"padding":"2px"});
+                td.append(keySpan);
+                row.append(td);
+
+                var descSpan = jQuery("<span></span>");
+                descSpan.append(keyText);
+
+                td = jQuery("<td></td>");
+                td.append(descSpan);
+                row.append(td);
+
+                table.append(row);
+              }
          }
     });
+    jQuery("body").append(container);
+    showFancyBox("#accessKeyMenuPopup");
 }
 jQuery(document).bind('keydown', 'Shift+h', showAccessKeys);
+
